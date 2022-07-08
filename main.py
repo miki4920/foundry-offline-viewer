31106c07d79d62d6c20ccf98e20eccb60b1dc978
miki4920 <miki4920@gmail.com>
2022-07-08 14:28:29 +0100

    Removed hardcoded numbers from html

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html", characters=["Lizzie", "Jean", "Reiner", "Quamnious", "Treasury"], current_character="All Characters")
+    characters_wealth_tuples = sorted([(149.73, "Jean"), (130.10, "Lizzie"), (100.50, "Quamnious"), (119.32, "Reiner"), (50.4, "Treasury")], reverse=True)
+    characters = [pair[1] for pair in characters_wealth_tuples]
+    wealth = [pair[0] for pair in characters_wealth_tuples]
+    return render_template("index.html", current_character="All Characters", characters=characters, wealth=wealth)
