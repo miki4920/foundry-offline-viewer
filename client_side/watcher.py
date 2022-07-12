597790c0b21cfb1b8b587a906f04ca94baaddbcc
miki4920 <miki4920@gmail.com>
2022-07-12 19:12:07 +0100

    Added file uploading within watcher

--- client_side/watcher.py
+++ client_side/watcher.py
@@ -1,14 +1,24 @@
+import os
+import requests
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-from update_file import db, CreateDatabase
+from common.model import db
+from update_file import CreateDatabase
+
+
+def send_database():
+    with open('../wealth.db', 'rb') as file:
+        requests.post(os.getenv("URL"), data=file)
 
 
 class FoundryHandler(FileSystemEventHandler):
-    def __init__(self, path, file_name):
-        self.file_name = file_name
+    def __init__(self):
+        path = os.getenv("FOUNDRY_PATH").split("/")
+        self.file_name = path[-1:][0]
         self.observer = Observer()
-        self.observer.schedule(self, path, recursive=False)
+        self.observer.schedule(self, "/".join(path[:-1]), recursive=False)
         self.observer.start()
         self.observer.join()
 
@@ -18,8 +28,8 @@ class FoundryHandler(FileSystemEventHandler):
             db.drop_all()
             db.create_all()
             CreateDatabase().insert_into_database(db)
+            send_database()
 
 
 if __name__ == "__main__":
-    event_handler = FoundryHandler("C:/Users/Mikolaj Grobelny/AppData/Local/FoundryVTT/Data/worlds/darklands/data/",
-                                   "actors.db")
+    event_handler = FoundryHandler()
