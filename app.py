e677b29bcf9fcd092266c2048939a00ea6a3e610
miki4920 <miki4920@gmail.com>
2022-07-13 23:59:37 +0100

    Changed app to be character based.

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
-from dynamodb_json import json_util as json
-from flask import render_template, Flask
+from flask import render_template, Flask, redirect
 
 from common.model import dynamodb
+from database_methods import fetch_characters
 
 character_colours = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)',
                      'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)',
                      'rgba(153, 102, 255, 0.2)']
 app = Flask("app")
-
-
-def get_batch_items(items):
-    response = dynamodb.batch_get_item(
-        RequestItems={
-            'items': {
-                'Keys': [{"id": {"N": str(item)}} for item in items],
-                'ConsistentRead': True
-            }
-        },
-        ReturnConsumedCapacity='TOTAL'
-    )["Responses"]["items"]
-    return json.loads(response)
 
 
 def unzip_list(zipped_list):
@@ -29,8 +16,9 @@ def unzip_list(zipped_list):
 def get_wealth(characters):
     wealth = []
     for i, character in enumerate(characters):
-        wealth.append((round(sum([item["value"] * item["quantity"] for item in character["items"]]), 2), character["name"],
-                       character_colours[i]))
+        wealth.append(
+            (round(sum([item["value"] * item["quantity"] for item in character["items"]]), 2), character["name"],
+             character_colours[i]))
     return unzip_list(wealth)
 
 
@@ -46,19 +34,20 @@ def get_wealth_without_consumables(characters):
 def get_highest_item_level(characters):
     highest_item_level = []
     for i, character in enumerate(characters):
-        highest_item_level.append((max([item["level"] for item in character["items"]]), character["name"], character_colours[i]))
+        highest_item_level.append(
+            (max([item["level"] for item in character["items"]]), character["name"], character_colours[i]))
     return unzip_list(highest_item_level)
 
 
 @app.route("/")
-def main_app():
-    characters = dynamodb.scan(TableName="characters")["Items"]
-    for character in characters:
-        character["name"] = json.loads(character["name"])
-        character["items"] = json.loads(character["items"])
-        character["items"] = get_batch_items(character["items"])
+def home():
+    return redirect("/Treasury")
+
+
+@app.route("/<current_character>")
+def main(current_character):
+    characters = fetch_characters()
     wealth = get_wealth(characters)
     wealth_without_consumable = get_wealth_without_consumables(characters)
-    character_names = [character["name"] for character in characters]
-    return render_template("index.html", characters=character_names, wealth=wealth,
+    return render_template("index.html", current_character=current_character, characters=characters, wealth=wealth,
                            wealth_without_consumable=wealth_without_consumable)
