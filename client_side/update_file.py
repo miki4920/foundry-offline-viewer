62d1c2419c18cecf55e1c055bc3343a192fcd0bb
miki4920 <miki4920@gmail.com>
2022-07-13 14:39:41 +0100

    Implemented entry creation

--- client_side/update_file.py
+++ client_side/update_file.py
@@ -1,16 +1,26 @@
 import json
 import os
+from decimal import Decimal
 
-from common.model import Character, Item
+from common.model import create_table
 
 
 class CreateDatabase:
-    def __init__(self):
+    def __init__(self, client):
         self.coin_dictionary = {"pp": 10, "gp": 1, "sp": 0.1, "cp": 0.01, }
         self.ids = ["AOh3phVs5PQ2Ae9A", "J5njm4YwaRu9sj3T", "MAi78zS5iDn4d5wp", "v9KEi6wvQBIMIAAA", "aLgv3EleWVLOWT68"]
         self.foundry_path = os.getenv("FOUNDRY_PATH")
-        self.properties_path = "../properties.json"
+        self.properties_path = os.getenv("PROPERTIES_PATH")
         self.properties = self.get_properties()
+        self.client = client
+        self.truncate_table("characters")
+        self.truncate_table("items")
+        create_table()
+
+    def truncate_table(self, table):
+        self.client.delete_table(TableName=table)
+        waiter = self.client.get_waiter('table_not_exists')
+        waiter.wait(TableName=table)
 
     def get_properties(self):
         with open(self.properties_path, "r") as file:
@@ -19,7 +29,7 @@ class CreateDatabase:
     def get_foundry_file(self):
         with open(self.foundry_path, "r") as file:
             text = file.readlines()
-            file = [json.loads(line) for line in text][::-1]
+        file = [json.loads(line) for line in text][::-1]
         return file
 
     def remove_duplicate_ids(self, json_file):
@@ -41,23 +51,33 @@ class CreateDatabase:
             gold_value += self.coin_dictionary[coin_type] * coin_quantity
         if data["price"].get("per"):
             gold_value /= data["price"]["per"]
-        return round(gold_value, 2), level
+        return json.loads(json.dumps(round(gold_value, 2)), parse_float=Decimal), level
 
-    def insert_into_database(self, db):
+    def insert_into_database(self):
         characters = self.remove_duplicate_ids(self.get_foundry_file())
+        count = 0
         for character in characters:
             items = character["items"]
-            character = Character(name=character["name"])
-            db.session.add(character)
+            item_ids = []
             for item in items:
                 if item["data"].get("price") and "infused" not in item["data"]["traits"]["value"]:
                     item_value, level = self.item_value_converter(item["data"])
                     consumable = "consumable" in item["data"]["traits"]["value"]
-                    item = Item(owner=character, name=item["name"], description=item["data"]["description"]["value"],
-                                level=level, value=item_value,
-                                quantity=item["data"]["quantity"], consumable=consumable)
-                    db.session.add(item)
-            db.session.commit()
+                    self.client.put_item(TableName='items', Item={
+                        'id': {"N": str(count)},
+                        'name': {"S": item["name"]},
+                        'description': {"S": item["data"]["description"]["value"]},
+                        'level': {"N": str(level)},
+                        'value': {"N": str(item_value)},
+                        'quantity': {"N": str(item["data"]["quantity"])},
+                        'consumable': {"N": str(int(consumable))}
+                    })
+                    item_ids.append(str(count))
+                    count += 1
+            self.client.put_item(TableName='characters', Item={
+                'name': {"S": character["name"]},
+                'items': {"NS": item_ids}
+            })
 
 
 
