f1ae0bdb1799fec02ec5720c2ca916674f08add1
miki4920 <miki4920@gmail.com>
2022-07-17 13:23:14 +0100

    Fixed an import crashing Heroku

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import render_template, Flask, redirect
-from database_methods import fetch_characters
+from common.model import fetch_characters
 
 character_colours = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)',
                      'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)',
