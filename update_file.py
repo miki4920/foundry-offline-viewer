344ea7b1709a11d96a9d13c6005fa173413face2
miki4920 <miki4920@gmail.com>
2022-08-18 20:52:13 +0100

    Updated wealth viewer to include armour property runes

--- update_file.py
+++ update_file.py
@@ -8,16 +8,16 @@ from database import truncate_table
 class CreateDatabase:
     def __init__(self, client):
         self.coin_dictionary = {"pp": 10, "gp": 1, "sp": 0.1, "cp": 0.01, }
-        self.ids = ["AOh3phVs5PQ2Ae9A", "J5njm4YwaRu9sj3T", "uW5Yzbb1H96P8Xid", "v9KEi6wvQBIMIAAA", "aLgv3EleWVLOWT68", "MAi78zS5iDn4d5wp"]
+        self.ids = ["AOh3phVs5PQ2Ae9A", "J5njm4YwaRu9sj3T", "v9KEi6wvQBIMIAAA", "aLgv3EleWVLOWT68", "MAi78zS5iDn4d5wp"]
         self.foundry_path = os.getenv("FOUNDRY_PATH")
-        self.properties_path = os.getenv("PROPERTIES_PATH")
         self.properties = self.get_properties()
         self.client = client
         truncate_table("characters")
         truncate_table("items")
 
-    def get_properties(self):
-        with open(self.properties_path, "r") as file:
+    @staticmethod
+    def get_properties():
+        with open(os.getenv("PROPERTIES_PATH"), "r") as file:
             return json.load(file)
 
     def get_foundry_file(self):
@@ -34,15 +34,28 @@ class CreateDatabase:
                 self.ids.remove(line["_id"])
         return unique_characters
 
-    def item_value_converter(self, data):
-        gold_value = 0
-        level = data["level"]["value"]
+    def check_if_item_has_runes(self, data, item_type):
+        if data.get("specific") is not None and data["specific"]["value"]:
+            return False
         for entry in data:
-            if self.properties.get(entry) and data[entry]["value"]:
-                gold_value += self.properties[entry][str(data[entry]["value"])]["price"]
-                level = max(self.properties[entry][str(data[entry]["value"])]["level"], level)
+            if self.properties.get(item_type) and self.properties[item_type].get(entry) and data[entry]["value"]:
+                return True
+        return False
+
+    def item_value_converter(self, data):
+        item_type = data["type"]
+        item_type = item_type if item_type == "armor" else "other"
+        data = data["data"]
+        level = data["level"]["value"]
+        gold_value = 0
         for coin_type, coin_quantity in data["price"]["value"].items():
             gold_value += self.coin_dictionary[coin_type] * coin_quantity
+        if self.check_if_item_has_runes(data, item_type):
+            gold_value = 0
+            for entry in data:
+                if self.properties.get(item_type) and self.properties[item_type].get(entry) and data[entry]["value"]:
+                    gold_value += self.properties[item_type][entry][str(data[entry]["value"])]["price"]
+                    level = max(self.properties[item_type][entry][str(data[entry]["value"])]["level"], level)
         if data["price"].get("per"):
             gold_value /= data["price"]["per"]
         return json.loads(json.dumps(round(gold_value, 2)), parse_float=Decimal), level
@@ -55,12 +68,11 @@ class CreateDatabase:
             item_ids = []
             for item in items:
                 if item["data"].get("price") and item["data"].get("quantity") and "infused" not in item["data"]["traits"]["value"]:
-                    item_value, level = self.item_value_converter(item["data"])
+                    item_value, level = self.item_value_converter(item)
                     consumable = "consumable" in item["data"]["traits"]["value"]
                     self.client.put_item(TableName='items', Item={
                         'id': {"N": str(count)},
                         'name': {"S": item["name"]},
-                        'description': {"S": item["data"]["description"]["value"]},
                         'level': {"S": str(level)},
                         'value': {"S": str(item_value)},
                         'quantity': {"S": str(item["data"]["quantity"])},
